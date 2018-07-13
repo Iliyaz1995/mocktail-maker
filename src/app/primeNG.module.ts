a72504851dd99b974c3bd9e23475137f44e73a64
Iliyaz Syed <eliyas.sd143@gmail.com>
2018-07-13 23:55:57 +0530

    make it better

--- src/app/primeNG.module.ts
+++ src/app/primeNG.module.ts
@@ -3,10 +3,10 @@ import { CdkTreeModule } from '@angular/cdk/tree';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AccordionModule } from 'primeng/components/accordion/accordion';
+import { AccordionModule } from 'primeng/accordion';
 import { PanelModule } from 'primeng/components/panel/panel';
-import { ButtonModule } from 'primeng/components/button/button';
-import { RadioButtonModule } from 'primeng/components/radioButton/radioButton';
+import { ButtonModule } from 'primeng/button';
+import { RadioButtonModule } from 'primeng/radiobutton';
 import { SidebarModule } from 'primeng/sidebar';
 import { TooltipModule } from 'primeng/tooltip';
 import { CheckboxModule } from 'primeng/checkbox';
